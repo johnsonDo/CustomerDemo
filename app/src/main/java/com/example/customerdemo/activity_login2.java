bcfdc1f79fec13e3f3e15a9ca08a37875a40d8b7
dongcheng jiang <dongcheng.jiang@siemens.com>
2019-09-09 09:53:09 +0800

    change customer login activity.

--- app/src/main/java/com/example/customerdemo/activity_login2.java
+++ app/src/main/java/com/example/customerdemo/activity_login2.java
@@ -2,7 +2,11 @@ package com.example.customerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_login2 extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class activity_login2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        Button button = findViewById(R.id.button2);
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ComponentName componetName = new ComponentName(
+                        //这个是另外一个应用程序的包名
+                        "com.example.myapplication",
+                        //这个参数是要启动的Activity
+                        "com.example.myapplication.MainActivity");
+                Intent intent= new Intent();
+                intent.setComponent(componetName);
+                startActivity(intent);
+                overridePendingTransition(R.animator.zoomout, R.animator.zoomin);
+
+                    }
+                }
+        );
     }
 }
